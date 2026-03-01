c59c538eb87316ceae97f0027b00734fb1756406
Joy Chetry <chetryjoy0@gmail.com>
2026-03-01 14:58:52 +0530

    feat: bump plugin version to 0.5 - prepares for next release cycle

--- dodo-payments-for-woocommerce.php
+++ dodo-payments-for-woocommerce.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://dodopayments.com
  * Short Description: Accept payments globally within minutes.
  * Description: Dodo Payments plugin for WooCommerce. Accept payments from your customers using Dodo Payments.
- * Version: 0.4
+ * Version: 0.5
  * Author: Dodo Payments
  * Developer: Dodo Payments
  * Text Domain: dodo-payments-for-woocommerce
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: ayushdodopayments
 Tags: payments, woocommerce, dodo
 Requires at least: 6.1
 Tested up to: 6.9
-Stable tag: 0.4
+Stable tag: 0.5
 License: GPLv3 or later
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
