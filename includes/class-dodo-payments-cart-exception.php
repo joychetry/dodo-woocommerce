2167c2d9d6f37d10af1d2d1f6bc948ad0aeda6d0
Joy Chetry <chetryjoy0@gmail.com>
2026-03-04 18:27:29 +0530

    chore: update nullable type hint in cart exception constructor - ensures PHP 8 compatibility

--- includes/class-dodo-payments-cart-exception.php
+++ includes/class-dodo-payments-cart-exception.php
@@ -8,7 +8,7 @@
  */
 class Dodo_Payments_Cart_Exception extends Exception
 {
-  public function __construct($message, $code = 0, Exception $previous = null)
+  public function __construct($message, $code = 0, ?Exception $previous = null)
   {
     parent::__construct($message, $code, $previous);
   }
